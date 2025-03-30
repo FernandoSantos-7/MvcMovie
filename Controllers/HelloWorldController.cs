44ba1756871da87ee12907077556ebb340107d47
Fernando Santos <santosfernandoe225@gmail.com>
2025-03-30 16:32:16 -0300

    actividad 2

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+using static System.Collections.Specialized.BitVector32;
+
+namespace Mvc_Movil.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+            
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+
+    }
+    
+}
