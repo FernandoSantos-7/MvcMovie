2da8a4ddba60cd2a3b42a8027400451f5e19005e
Fernando Santos <santosfernandoe225@gmail.com>
2025-04-06 16:42:12 -0300

    Modifique el nuevo proyecto de P5

---
+++ Controllers/HolaMundoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCPeliculas.Controllers
+{
+    public class HolaMundoController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Titulo"] = "Este es mi Titulo desde mi controlador";
+            return View();
+        }
+
+
+        public IActionResult Welcome(string name, int Count = 1)
+        {
+            ViewData["Message"] = "bienvenido a mi controlador" +name;
+            ViewData["Count"] = Count;
+
+
+            return View();
+        }
+
+
+    }
+}
