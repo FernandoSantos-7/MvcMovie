eea3dd8b0e1d516e2fe5e9864b9b3d7f6ec08be6
Fernando Santos <santosfernandoe225@gmail.com>
2025-04-06 16:37:59 -0300

    modifique un nuevo proyecto el otro lo borre

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mvc_Movil.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
